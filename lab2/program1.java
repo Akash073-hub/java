7ae31477f5a46aa2c292795572709f5a9a65a09c
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2025-08-19 10:39:38 +0530

    program1.java

--- lab2/program1.java
+++ lab2/program1.java
@@ -1,1 +1,17 @@
+package program1;
+
+public class program1 {
+	String name;
+	int age;
+	
+	void displayinfo() {
+		System.out.println("Name "+ name +"\n"  + " Age " +age);
+	}
+	public static void main(String[] args) {
+		 program1 s1=new program1();
+		 s1.name ="john";
+		 s1.age = 10;
+		 s1.displayinfo();
+	}
+}
 
